dbb2a9e4d73461f0a9a22d1f5b2c4ef64717450d
Gabe <gabe1118@yahoo.com>
2015-08-15 00:34:51 -0700

    More functions

--- rwfb.py
+++ rwfb.py
@@ -1,4 +1,3 @@
-
 import mongoconn
 
 NOT_FOUND = "No Player Found"
@@ -16,22 +15,59 @@ def query(db, number):
         value = cursor[0]
     return value
 
+def tostr(pl):
+    return str(pl["key"][-2:]) + " hp:" + str(pl["currenthp"])
+
+def getEntities(db, loc, radius):
+    obj = []
+    #TODO FIX COORDS
+    coords = [(x,y) 
+        for x in range(loc[0]-radius, loc[0]+radius+1)
+        for y in range(loc[1]-radius, loc[1]+radius+1)
+        if ((x-loc[0])**2 + (y-loc[1])**2) <= radius**2
+    ]
+    for x,y in coords:
+        #print(x,y)
+        qri = {"location": [x,y]}
+        pcursor = db["players"].find(qri)
+        # ecursor = db["enemies"].find(qri)
+        for pl in pcursor:
+            obj.append(tostr(pl))
+    return obj
+                
 def getItemStats(db, itemname):
-    return ""
+    qri = {"key": itemname}
+    cursor = db["items"].find(qri)
+    value = None
+    if cursor.count() != 0:
+        #we found something
+        value = cursor[0]
+    return value
+
+def createItem(db, item, atk, blk):
+    db["items"].insert({
+        "name":str(item),
+        "attack":str(atk),
+        "defense":str(blk),
+    })
+    return "Item Created"
 
 def createPlayer(db, number, name):
+    if(query(db, number) != None):
+        return
+
     db["players"].insert({
-        key: number,
-        loggedon: true,
-        location: [0,0],
-        isdead: false,
-        currenthp: 10,
-        maxhp: 10,
-        level: 1,
-        attack: 10,
-        def: 10, 
-        equipment: [],
-        inventory: []
+        "key": number,
+        "loggedon": "true",
+        "location": [0,0],
+        "isdead": "false",
+        "currenthp": "10",
+        "maxhp": "10",
+        "level": "1",
+        "attack": "10",
+        "defense": "10", 
+        "equipment": [],
+        "inventory": []
     })
     return
 
@@ -45,13 +81,36 @@ def applyDamage(db, number, dmg):
         #playerdies
         return DEAD_PLYR
     else:
+        player["currenthp"] = cur-dmg
         db["players"].update(
-            {'_id':doc["_id"]}, 
-            {"currenthp": cur-dmg},
+            {'_id':player["_id"]}, 
+            player,
             upsert=False)
 
     return "Update Successful: " + str(cur-dmg)
-            
+     
+def getLocation(db, number):
+    player = query(db, number)
+    if(player == None):
+        return NOT_FOUND
+    return player["location"]
+
+def updateLocation(db, number, dx=0, dy=0):
+    player = query(db, number)
+    if(player == None):
+        return NOT_FOUND
+
+    cur = player["location"]
+    loc = [cur[0]+dx, cur[1]+dy]
+
+    player["location"] = loc
+    db["players"].update(
+        {'_id':player["_id"]}, 
+        player,
+        upsert=False)
+
+    return "Update Successful: " + str(loc)
+
 def listItems(db, number):
     player = query(db, number)
     if(player == None):
