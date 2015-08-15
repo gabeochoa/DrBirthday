f3a71a0d50ec88dd8cb61e4ea9155edfab3b0900
Gabe <gabe1118@yahoo.com>
2015-08-14 21:57:03 -0700

    Twilio Basic

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import twilio.twiml
+
 app = Flask(__name__)
 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
 
 if __name__ == "__main__":
     app.run()
